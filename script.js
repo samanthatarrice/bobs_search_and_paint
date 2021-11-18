4b949f65d6c0e9991503aff8b9c55cdf3adb3dee
Samantha Tarrice <s.tarrice@gmail.com>
2021-11-18 16:48:37 -0300

    Canvas under tv is workinggit add .

--- script.js
+++ script.js
@@ -10,13 +10,17 @@ const canvas = document.querySelector(".canvas");
 const ctx = canvas.getContext('2d');
 
 window.addEventListener('load', () => {
-  canvas.width = window.innerWidth/1.4;
-  canvas.height = windew.innerHeight/1.5;
+  canvas.width = window.innerWidth/1.8;
+  canvas.height = canvas.width * 0.82;
 })
 
 window.addEventListener('resize', () => {
-  canvas.width = window.innerWidth/1.4;
-  canvas.height = window.innerHeight/1.5;
+  canvas.width = window.innerWidth/1.8;
+  canvas.height = canvas.width * 0.82;
 })
 
-// canvas.style.margin = '0 auto';
+canvas.style.zIndex = '3';
+
+const tv = document.querySelector('.tv');
+
+tv.style.zIndex = '4';
